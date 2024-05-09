86c8c77d55d7cba9ff3ec5c1f8268de14d1a8f40
Ansh <anshraj7@outlook.com>
2024-05-09 12:18:50 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'whatsapp-tg' : '#00a884',
+        'whatsapp-fg' : '#0c1317',
+      },
+    },
+  },
+  plugins: [],
+}
+
